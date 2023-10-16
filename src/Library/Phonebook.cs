6d1cc20bfa082030e3b974824f7b5002f2726dbd
Manuela Guedez <manuela.guedez@correo.ucu.edu.uy>
2023-10-16 16:34:26 -0300

    added AddContact method

--- src/Library/Phonebook.cs
+++ src/Library/Phonebook.cs
@@ -31,5 +31,32 @@ namespace Library
 
             return result;
         }
+
+        public void AddContact(string name, string phone, string email)
+        {
+            // Por Creator
+            Contact person = new Contact(name);
+            person.Phone = phone;
+            person.Email = email;
+            
+            this.persons.Add(person);
+        }
+
+        public void RemoveContact(string name,string phone, string email)
+        {
+            // Esta podría ser una posible implementación para el método
+            foreach(Contact person in this.persons)
+            {
+                if (person.Name.Equals(name) && person.Phone.Equals(phone) && person.Email.Equals(email))
+                {
+                    persons.Remove(person);
+                }
+            }
+        }
+
+        public void SendMessage()
+        {
+            
+        }
     }
 }
